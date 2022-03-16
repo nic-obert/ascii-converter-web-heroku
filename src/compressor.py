cf162038ec33d16db14883359ea849860e7f9fe2
Nicholas Obert <nchlsuba@gmail.com>
2022-03-16 13:55:45 +0100

    reritten compression and decompression algorithm

--- src/compressor.py
+++ src/compressor.py
@@ -46,41 +46,36 @@ def compress_ascii_image(image: ASCIIImage) -> bytes:
     # First create an digest with the image header
     digest = bytearray(generate_header(image))
     data = image.data[5:]
-    current_char = ''
+    
     count = 1
+    current_char = None
     for char in data:
 
+        # Newline character resets the count
+        # The last character of an image should always be a newline
         if char == '\n':
-
-            if count == 1:
-                digest.append(ord(current_char))
-            elif count < 4:
-                digest.extend([ord(current_char)] * count)
+            if count < 4:
+                digest.extend([current_char] * count)
             else:
                 digest.extend(multi_char(current_char, count))
-            
             count = 1
-            current_char = ''
-        
-        elif char == current_char:
-            count += 1
+            current_char = None
 
+        elif current_char == char:
+            count += 1
             if count == 255:
-                digest.extend(multi_char(char, 255))
+                digest.extend(multi_char(current_char, 255))
                 count = 1
+        
+        elif count < 4:
+            if current_char is not None:
+                digest.extend([current_char] * count)
+                count = 1
+            current_char = char
 
         else:
-
-            if current_char != '':
-
-                if count == 1:
-                    digest.append(ord(current_char))
-                elif count < 4:
-                    digest.extend([ord(current_char)] * count)
-                else:
-                    digest.extend(multi_char(current_char, count))
-                    count = 1
-            
+            digest.extend(multi_char(current_char, count))
+            count = 1
             current_char = char
 
     return bytes(digest)
@@ -91,34 +86,37 @@ def decompress_ascii_image(data: bytes) -> ASCIIImage:
 
     data = data[5:]
 
-    ascii_data = bytearray([NEWLINE_CHAR_CODE] * (width * height))
+    # Create a buffer to store the decompressed image. 
+    # Width + 1 is because we need to store the newline character.
+    # The buffer is filled with newline characters to ensure that the image has newlines after each row.
+    ascii_image = bytearray([NEWLINE_CHAR_CODE] * ((width + 1) * height))
 
-    # Index is -1 because at the first iteration, the index will be incremented
-    ascii_index = 0
-    byte_index = 0
-    upper_bound = len(data)
-    while byte_index < upper_bound:
+    data_index = 0
+    image_index = 0
+    while data_index < len(data):
+        byte = data[data_index]
 
-        # Skip one byte for the newline character
-        if ascii_index % (width) == 0 and byte_index != 0:
-            ascii_index += 1
+        # Check if the end of the row has been reached
+        # width - 1 is because indices start at 0
+        if image_index % (width - 1) == 0:
+            image_index += 1
 
-        if data[byte_index] == MULTI_CHAR_SIGN:
-            count = data[byte_index + 1]
-            char = data[byte_index + 2]
-            ascii_data[ascii_index:ascii_index + count] = [char] * count
-            ascii_index += count
-            byte_index += 3
-
+        # If the byte is a multi-character sequence, read the count and the character
+        if byte == MULTI_CHAR_SIGN:
+            count = data[data_index + 1]
+            char = data[data_index + 2]
+            data_index += 3 # Skip the whole multi-char sequence
+            ascii_image[image_index:image_index + count] = [char] * count
+            image_index += count
+        
+        # If the byte is a single character, just copy it
         else:
-            ascii_data[ascii_index] = data[byte_index]
-            ascii_index += 1
-            byte_index += 1
-
-    print("length: ", len(ascii_data))
-    print("index of newline: ", ascii_data.index(NEWLINE_CHAR_CODE))
+            ascii_image[image_index] = byte
+            image_index += 1
+            data_index += 1
+    
     return ASCIIImage(
-        data=ascii_data.decode('ascii'),
+        data=ascii_image.decode('ascii'),
         width=width,
         height=height,
         style_code=style_code
